f2ef3d28e695bcd1e350d3b612c7f0f886bc97f2
getmesomecoff33 <getmesomecoff33@gmail.com>
2023-11-17 10:15:16 +0100

    CHG: Removed Debug Prints  Changes to be committed: 	modified:   designObject.js

--- designObject.js
+++ designObject.js
@@ -6,16 +6,10 @@ let providerPublicKey = null;
 exports.authenticate = authenticate;
 
 function authenticate(authInfo, context) {
-    print("__authenticate__")
-    print(Object.getOwnPropertyNames(authInfo))
-    print(authInfo.username)
-    print(authInfo.token)
-    print(Object.getOwnPropertyNames(context))
     cacheKeyIfNeeded();
     if (isTokenAuthentication(authInfo)) {
         return checkCredentials(authInfo);
     } else {
-        print("__return null__")
         return null;
     }
 }
@@ -30,34 +24,23 @@ function isTokenAuthentication(authInfo) {
 }
 
 function isJwtFromProvider(token) {
-    print("__isJwtFromProvider__")
     try  {
         if (!token.includes(".")){
-            print("token does not include .")
-            print(token)
             return false
         }
         const claims = cordraUtil.extractJwtPayload(token);    
-        print("verify claims")
         if ("https://accounts.google.com" == claims.iss){
             return true
         }
     } catch (error) {
-        print(error)
         return false;
     }
 }
 
 function checkCredentials(authInfo) {
-    print("__enter checkCredentials__")
-    print("__Good till here___")
-    print(Object.getOwnPropertyNames(authInfo))
     const token = authInfo.token;
     const payload = cordraUtil.extractJwtPayload(token);
-    print(Object.getOwnPropertyNames(payload))
     const isVerified = true;
-    print("isVerified:")
-    print(isVerified)
     const claimsCheck = checkClaims(payload);
     const active = isVerified && claimsCheck;
     const result = {
@@ -76,12 +59,6 @@ function checkCredentials(authInfo) {
         }
         result.grantAuthenticatedAccess = true;
     }
-    print("_______________")
-    print(Object.getOwnPropertyNames(result))
-    print(result.userId)
-    print(result.active)
-    print(result.exp)
-    print(result.grantAuthenticatedAccess)
     return result;
 }
 
@@ -98,48 +75,34 @@ function getTokenFromAuthHeader(authHeader) {
 }
 
 function checkClaims(claims) {
-    print("__check Claims__")
     if (!claims.iss || !claims.exp || !claims.aud) {
-        print("return in first")
         return false;
     }
     if ("https://accounts.google.com" !== claims.iss) {
-        print("returen in second")
         return false;
     }
     const nowInSeconds = Math.floor(Date.now() / 1000);
     if (nowInSeconds > claims.exp) {
-        print("return in third")
         return false;
     }
     const aud = claims.aud;
     print(aud)
     if (!checkAudience(aud)) {
-        print("return in fourth")
         return false;
     }
-    print("its ture baby")
     return true;
 }
 
 function checkAudience(audElement) {
-    print("__check AUdience__")
     let aud = [];
     if (typeof audElement === "string") {
         aud.push(audElement);
     } else if (Array.isArray(audElement)) {
         aud = audElement;
     } else {
-        print("__return in first")
         return false;
     }
     return true
-    print(aud.includes(".apps.googleusercontent.com"))
-    if (aud.includes("https://cordra.org/")) {
-        return true;
-    } else {
-        return false;
-    }
 }
 
 function cacheKeyIfNeeded() {
@@ -149,7 +112,6 @@ function cacheKeyIfNeeded() {
         const keyPath = configDir + File.separator + "publicKey.jwk";
         providerPublicKey =  cordraUtil.getCordraPublicKey()
         return true
-        providerPublicKey = readFileToJsonAndParse(keyPath);
     }
 }
 
